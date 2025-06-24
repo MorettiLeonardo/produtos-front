d3412363a016433023ef017cfeae3b0ec58ae7f9
Leonardo Moretti <leonardonabarro2003@gmail.com>
2025-06-24 09:22:51 -0300

    feat: configurando react router dom

---
+++ src/routes/PageRoutes.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from '../screens/Home'
+
+const PagesRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default PagesRoutes
